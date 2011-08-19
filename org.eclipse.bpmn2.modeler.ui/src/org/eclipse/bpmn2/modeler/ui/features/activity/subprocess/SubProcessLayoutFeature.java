9988e12b668a69e3d2bc38e2ecb73437f2f83cbc
Bob Brodt <bbrodt@redhat.com>
2011-08-19 16:06:15 -0600

    Partial fix for JBPM-3095 - constrain size of expanded subprocess

--- org.eclipse.bpmn2.modeler.ui/src/org/eclipse/bpmn2/modeler/ui/features/activity/subprocess/SubProcessLayoutFeature.java
+++ org.eclipse.bpmn2.modeler.ui/src/org/eclipse/bpmn2/modeler/ui/features/activity/subprocess/SubProcessLayoutFeature.java
@@ -15,13 +15,19 @@ package org.eclipse.bpmn2.modeler.ui.features.activity.subprocess;
 import org.eclipse.bpmn2.Activity;
 import org.eclipse.bpmn2.modeler.core.features.activity.ActivityLayoutFeature;
 import org.eclipse.graphiti.features.IFeatureProvider;
+import org.eclipse.graphiti.features.context.ILayoutContext;
 import org.eclipse.graphiti.mm.algorithms.GraphicsAlgorithm;
 import org.eclipse.graphiti.mm.algorithms.Text;
+import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.eclipse.graphiti.mm.pictograms.PictogramElement;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.eclipse.graphiti.services.Graphiti;
 
 public class SubProcessLayoutFeature extends ActivityLayoutFeature {
 
+	private final static int MARGIN = 2;
+	private final static int NUMBER_OF_PARENT_ELEMENTS = 3;
+	
 	public SubProcessLayoutFeature(IFeatureProvider fp) {
 		super(fp);
 	}
@@ -33,5 +39,91 @@ public class SubProcessLayoutFeature extends ActivityLayoutFeature {
 			return true;
 		}
 		return false;
+	}
+	
+	@Override
+	public boolean layout(ILayoutContext context) {
+		ContainerShape containerShape = (ContainerShape) context.getPictogramElement();
+		GraphicsAlgorithm parentGa = containerShape.getGraphicsAlgorithm();
+		int newWidth = parentGa.getWidth();
+		int newHeight = parentGa.getHeight();
+
+		int minX = Integer.MAX_VALUE;
+		int minY = Integer.MAX_VALUE;
+		int minWidth = 0;
+		int minHeight = 0;
+		int size = containerShape.getChildren().size();
+		for (int i=NUMBER_OF_PARENT_ELEMENTS; i<size; ++i) {
+			PictogramElement pe = containerShape.getChildren().get(i);
+			GraphicsAlgorithm ga = pe.getGraphicsAlgorithm();
+			if (ga!=null) {
+				int x = ga.getX();
+				int y = ga.getY();
+				if (x < minX)
+					minX = x;
+				if (y < minY)
+					minY = y;
+			}
+		}
+		for (int i=NUMBER_OF_PARENT_ELEMENTS; i<size; ++i) {
+			PictogramElement pe = containerShape.getChildren().get(i);
+			GraphicsAlgorithm ga = pe.getGraphicsAlgorithm();
+			if (ga!=null) {
+				int w = ga.getX() - minX + ga.getWidth();
+				int h = ga.getY() - minY + ga.getHeight();
+				if (w > minWidth)
+					minWidth = w;
+				if (h > minHeight)
+					minHeight = h;
+			}
+		}
+		
+		if (minX < MARGIN)
+			minX = MARGIN;
+		if (minY < MARGIN)
+			minY = MARGIN;
+		minWidth += 2 * MARGIN;
+		minHeight += 2 * MARGIN;
+		
+		if (newWidth < minWidth) {
+			parentGa.setWidth(minWidth);
+		}
+		if (newWidth < minX + minWidth) {
+			int shift = minX + minWidth - newWidth;
+			if (shift>minX-MARGIN) {
+				shift = minX-MARGIN;
+			}
+			if (shift>0) {
+				for (int i=NUMBER_OF_PARENT_ELEMENTS; i<size; ++i) {
+					PictogramElement pe = containerShape.getChildren().get(i);
+					GraphicsAlgorithm ga = pe.getGraphicsAlgorithm();
+					if (ga!=null) {
+						int x = ga.getX() - shift;
+						ga.setX(x);
+					}
+				}
+			}
+		}
+		if (newHeight < minHeight) {
+			parentGa.setHeight(minHeight);
+		}
+		if (newHeight < minY + minHeight) {
+			int shift = minY + minHeight - newHeight;
+			if (shift>minY-MARGIN) {
+				shift = minY-MARGIN;
+			}
+			if (shift>0) {
+				for (int i=NUMBER_OF_PARENT_ELEMENTS; i<size; ++i) {
+					PictogramElement pe = containerShape.getChildren().get(i);
+					GraphicsAlgorithm ga = pe.getGraphicsAlgorithm();
+					if (ga!=null) {
+						int y = ga.getY() - shift;
+						ga.setY(y);
+					}
+				}
+			}
+		}
+
+		return super.layout(context);
 	}
 }
