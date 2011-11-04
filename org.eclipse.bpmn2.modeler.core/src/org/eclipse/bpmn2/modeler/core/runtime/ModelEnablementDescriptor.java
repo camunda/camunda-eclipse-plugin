6fb821207f58f6cb8ab2b4f10058c8ef4d19c3cc
Bob Brodt <bbrodt@redhat.com>
2011-11-04 11:04:31 -0600

    First shot at modelEnablement runtime extension to limit visibility of BPMN2 elements and features - forgot to add Definitions for default enablement

---
+++ org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/core/runtime/ModelEnablementDescriptor.java
@@ -0,0 +1,229 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ *  All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ *
+ * @author Bob Brodt
+ ******************************************************************************/
+
+package org.eclipse.bpmn2.modeler.core.runtime;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map.Entry;
+
+import org.eclipse.bpmn2.Bpmn2Package;
+import org.eclipse.bpmn2.di.BpmnDiPackage;
+import org.eclipse.emf.common.util.EList;
+import org.eclipse.emf.ecore.EAttribute;
+import org.eclipse.emf.ecore.EClass;
+import org.eclipse.emf.ecore.EClassifier;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EReference;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+/**
+ * @author Bob Brodt
+ *
+ */
+public class ModelEnablementDescriptor extends BaseRuntimeDescriptor {
+
+	private Hashtable<String, HashSet<String>> classes = new Hashtable<String, HashSet<String>>();
+	
+	// require a TargetRuntime!
+	private ModelEnablementDescriptor() {
+	}
+	
+	public ModelEnablementDescriptor(TargetRuntime rt) {
+		super(rt);
+		setEnabledAll(true);
+	}
+
+	private void setEnabledAll(boolean enabled) {
+		if (enabled) {
+			Bpmn2Package i = Bpmn2Package.eINSTANCE;
+			setEnabled(getSubClasses(i.getFlowElement()), true);
+			setEnabled(getSubClasses(i.getDataAssociation()), true);
+			setEnabled(getSubClasses(i.getRootElement()), true);
+			setEnabled(getSubClasses(i.getEventDefinition()), true);
+			setEnabled(getSubClasses(i.getLoopCharacteristics()), true);
+			setEnabled(getSubClasses(i.getExpression()), true);
+			setEnabled(i.getDefinitions(), true);
+			setEnabled(i.getOperation(), true);
+			setEnabled(i.getLane(), true);
+			setEnabled(i.getEscalation(), true);
+			setEnabled(i.getPotentialOwner(), true);
+			setEnabled(i.getResourceAssignmentExpression(), true);
+			setEnabled(i.getInputSet(), true);
+			setEnabled(i.getOutputSet(), true);
+			setEnabled(i.getAssignment(), true);
+			setEnabled(i.getAssociation(), true);
+			setEnabled(i.getTextAnnotation(), true);
+			setEnabled(i.getMessageFlow(), true);
+			setEnabled(i.getConversationLink(), true);
+			setEnabled(i.getGroup(), true);
+			setEnabled(i.getConversation(), true);
+		}
+		else {
+			classes.clear();
+		}
+	}
+	
+	private void setEnabled(List<EClass> eClasses, boolean enabled) {
+		for (EClass c : eClasses) {
+			setEnabled(c,enabled);
+		}
+	}
+	
+	public void setEnabled(EClass eClass, boolean enabled) {
+		if (eClass.getInstanceClass() != EObject.class) {
+			if (enabled) {
+				if (!classes.containsKey(eClass.getName()))
+					setEnabled(eClass.getName(), true);
+			}
+			else {
+				setEnabled(eClass.getName(), false);
+			}
+		}
+	}
+	
+	private EClass getEClass(String className) {
+		// try the runtime package first
+		EClass eClass = (EClass)getEPackage().getEClassifier(className);
+		// then all BPMN2 packages
+		if (eClass==null)
+			eClass = (EClass)Bpmn2Package.eINSTANCE.getEClassifier(className);
+		if (eClass==null)
+			eClass = (EClass)BpmnDiPackage.eINSTANCE.getEClassifier(className);
+//		if (eClass==null)
+//			eClass = (EClass)DcPackage.eINSTANCE.getEClassifier(className);
+//		if (eClass==null)
+//			eClass = (EClass)DiPackage.eINSTANCE.getEClassifier(className);
+		return eClass;
+	}
+	
+	public void setEnabled(String className, boolean enabled) {
+		EClass eClass = getEClass(className);
+		if (eClass==null)
+			return;
+		
+		// add an entry for the class name by itself
+		if (enabled) {
+			if (classes.containsKey(className))
+				return;
+			HashSet<String> features = new HashSet<String>();
+			classes.put(className, features);
+			for (EAttribute a : eClass.getEAllAttributes()) {
+				features.add(a.getName());
+			}
+			for (EReference a : eClass.getEAllContainments()) {
+				features.add(a.getName());
+				setEnabled(a.getEReferenceType(), true);
+			}
+			for (EReference a : eClass.getEAllReferences()) {
+				features.add(a.getName());
+				setEnabled(a.getEReferenceType(), true);
+			}
+		}
+		else {
+			if (!classes.containsKey(className))
+				return;
+			classes.remove(className);
+
+			// remove any reference or containment list features
+			// of this type for other elements 
+			List<String> removed = new ArrayList<String>();
+			for (Entry<String, HashSet<String>> entry : classes.entrySet()) {
+				EClass ec = getEClass(entry.getKey());
+				HashSet<String> features = entry.getValue();
+
+				for (EReference a : ec.getEAllContainments()) {
+					// if this feature is a reference to the
+					// class being disabled, remove it
+					if (a.getEReferenceType() == eClass)
+						removed.add(a.getName());
+				}
+				for (EReference a : ec.getEAllReferences()) {
+					if (a.getEReferenceType() == eClass)
+						removed.add(a.getName());
+				}
+				features.removeAll(removed);
+			}
+		}
+	}
+	
+	public void setEnabled(String className, String featureName, boolean enabled) {
+		if ("all".equals(className))
+			setEnabledAll(enabled);
+		else if (featureName!=null && !featureName.isEmpty()) {
+			if (enabled) {
+				HashSet<String> features;
+				if (classes.containsKey(className)) {
+					features = classes.get(className);
+				}
+				else {
+					features = new HashSet<String>();
+					classes.put(className, features);
+				}
+				features.add(featureName);
+			}
+			else {
+				if (classes.containsKey(className)) {
+					classes.get(className).remove(featureName);
+				}
+			}
+		}
+		else
+			setEnabled(className, enabled);
+	}
+
+	public boolean isEnabled(String className, String featureName) {
+		if (classes.containsKey(className)) {
+			if (featureName!=null && !featureName.isEmpty()) {
+				return classes.get(className).contains(featureName);
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean isEnabled( EClass eClass, EStructuralFeature featureName) {
+		return isEnabled(eClass.getName(), featureName.getName());
+	}
+	
+	public Collection<String> getAllEnabled() {
+		return classes.keySet();
+	}
+	
+	public Collection<String> getAllEnabled(String className) {
+		if (classes.containsKey(className))
+			return classes.get(className);
+		return null;
+	}
+	
+	public static List<EClass> getSubClasses(EClass parentClass) {
+
+		List<EClass> classList = new ArrayList<EClass>();
+		EList<EClassifier> classifiers = Bpmn2Package.eINSTANCE.getEClassifiers();
+
+		for (EClassifier classifier : classifiers) {
+			if (classifier instanceof EClass) {
+				EClass clazz = (EClass) classifier;
+
+				clazz.getEAllSuperTypes().contains(parentClass);
+				if (parentClass.isSuperTypeOf(clazz) && !clazz.isAbstract()) {
+					classList.add(clazz);
+				}
+			}
+		}
+		return classList;
+	}
+}
