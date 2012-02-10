53992647705164ae40c73081a44d15984b32daec
Bob Brodt <bbrodt@redhat.com>
2012-02-10 07:39:50 -0700

    https://issues.jboss.org/browse/JBPM-3535 - ResourceRole list replaced with a single text field that creates a PotentialOwner

--- org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/JbpmUserTaskPropertiesComposite.java
+++ org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/JbpmUserTaskPropertiesComposite.java
@@ -13,7 +13,13 @@
 
 package org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5.property;
 
+import org.eclipse.bpmn2.PotentialOwner;
+import org.eclipse.bpmn2.ResourceRole;
+import org.eclipse.bpmn2.UserTask;
 import org.eclipse.bpmn2.modeler.ui.property.AbstractBpmn2PropertySection;
+import org.eclipse.bpmn2.modeler.ui.property.editors.TextObjectEditor;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
 
 /**
@@ -30,4 +36,18 @@ public class JbpmUserTaskPropertiesComposite extends JbpmTaskPropertiesComposite
 		super(parent, style);
 	}
 
+	@Override
+	public void createBindings(EObject be) {
+		UserTask task = (UserTask)be;
+		PotentialOwner owner = null;
+		if (task.getResources().size() == 0) {
+			owner = getDiagramEditor().getModelHandler().createStandby(
+					task, PACKAGE.getActivity_Resources(), PotentialOwner.class);
+		}
+		else if (task.getResources().get(0) instanceof PotentialOwner){
+			owner = (PotentialOwner)task.getResources().get(0);
+		}
+		TextObjectEditor editor = new TextObjectEditor(this, owner, PACKAGE.getResourceRole_Name());
+		editor.createControl("Actor");
+	}
 }
