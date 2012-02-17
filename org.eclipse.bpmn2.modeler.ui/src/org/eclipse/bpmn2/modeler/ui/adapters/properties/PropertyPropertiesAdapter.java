6f3b78d1f812afe3857a2f3b902714144cc8fe52
Bob Brodt <bbrodt@redhat.com>
2012-02-17 14:41:35 -0700

    oops - forgot some new classes

---
+++ org.eclipse.bpmn2.modeler.ui/src/org/eclipse/bpmn2/modeler/ui/adapters/properties/PropertyPropertiesAdapter.java
@@ -0,0 +1,94 @@
+/*******************************************************************************
+ * Copyright (c) 2011 Red Hat, Inc.
+ *  All rights reserved.
+ * This program is made available under the terms of the
+ * Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Red Hat, Inc. - initial API and implementation
+ *
+ * @author Bob Brodt
+ ******************************************************************************/
+
+package org.eclipse.bpmn2.modeler.ui.adapters.properties;
+
+import java.util.ArrayList;
+
+import org.eclipse.bpmn2.Activity;
+import org.eclipse.bpmn2.Bpmn2Package;
+import org.eclipse.bpmn2.Participant;
+import org.eclipse.bpmn2.Process;
+import org.eclipse.bpmn2.Property;
+import org.eclipse.bpmn2.modeler.core.adapters.ExtendedPropertiesAdapter;
+import org.eclipse.bpmn2.modeler.core.adapters.FeatureDescriptor;
+import org.eclipse.bpmn2.modeler.core.adapters.ObjectDescriptor;
+import org.eclipse.bpmn2.modeler.ui.adapters.properties.MultiInstanceLoopCharacteristicsPropertiesAdapter.LoopCharacteristicsDataInputFeatureDescriptor;
+import org.eclipse.bpmn2.modeler.ui.util.PropertyUtil;
+import org.eclipse.emf.common.notify.AdapterFactory;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.emf.ecore.EStructuralFeature;
+
+/**
+ * @author Bob Brodt
+ *
+ */
+public class PropertyPropertiesAdapter extends ExtendedPropertiesAdapter {
+
+	/**
+	 * @param adapterFactory
+	 * @param object
+	 */
+	public PropertyPropertiesAdapter(AdapterFactory adapterFactory, EObject object) {
+		super(adapterFactory, object);
+
+    	EStructuralFeature f = Bpmn2Package.eINSTANCE.getProperty_Name();
+		final FeatureDescriptor fd = new FeatureDescriptor(adapterFactory,object, f) {
+
+			@Override
+			public void setText(String text) {
+				super.setText(text);
+			}
+
+			// TODO: fix this!
+//			@Override
+//			public String getText(Object context) {
+//				Property property = (Property)(context instanceof Property ? context : this.object);
+//				String text = property.getName();
+//				if (text==null || text.isEmpty())
+//					text = property.getId();
+//				
+//				EObject container = property.eContainer();
+//				while (container!=null) {
+//					if (container instanceof Participant) {
+//						container = ((Participant)container).getProcessRef();
+//						if (container==null)
+//							break;
+//					}
+//					if (container instanceof Activity || container instanceof Process) {
+//						text = PropertyUtil.getText(container) + "/" + text;
+//					}
+//					container = container.eContainer();
+//				}
+//				return text;
+//			}
+			
+		};
+		setFeatureDescriptor(f, fd);
+		
+		setObjectDescriptor(new ObjectDescriptor(adapterFactory, object) {
+
+			@Override
+			public void setText(String text) {
+				fd.setText(text);
+			}
+
+			@Override
+			public String getText(Object context) {
+				return fd.getText(context);
+			}
+			
+		});
+	}
+
+}
