aa789caf0af776a47d237a48402d6e75afce600b
Bob Brodt <bbrodt@redhat.com>
2012-04-16 14:02:24 -0600

    https://bugs.eclipse.org/bugs/show_bug.cgi?id=376912 - needed a couple more levels of indirection

--- org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/JbpmUserTaskPropertiesComposite.java
+++ org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5/src/org/eclipse/bpmn2/modeler/runtime/jboss/jbpm5/property/JbpmUserTaskPropertiesComposite.java
@@ -13,7 +13,9 @@
 
 package org.eclipse.bpmn2.modeler.runtime.jboss.jbpm5.property;
 
+import org.eclipse.bpmn2.FormalExpression;
 import org.eclipse.bpmn2.PotentialOwner;
+import org.eclipse.bpmn2.ResourceAssignmentExpression;
 import org.eclipse.bpmn2.UserTask;
 import org.eclipse.bpmn2.modeler.ui.property.AbstractBpmn2PropertySection;
 import org.eclipse.bpmn2.modeler.ui.property.editors.TextObjectEditor;
@@ -43,14 +45,24 @@ public class JbpmUserTaskPropertiesComposite extends JbpmTaskPropertiesComposite
 			if (modelEnablement.isEnabled(object.eClass(), feature)) {
 				UserTask task = (UserTask)object;
 				PotentialOwner owner = null;
+				ResourceAssignmentExpression resourceAssignment = null;
+				FormalExpression expression = null;
 				if (task.getResources().size() == 0) {
 					owner = getDiagramEditor().getModelHandler().createStandby(
 							task, PACKAGE.getActivity_Resources(), PotentialOwner.class);
+					resourceAssignment = getDiagramEditor().getModelHandler().createStandby(
+							owner, PACKAGE.getResourceRole_ResourceAssignmentExpression(),
+							ResourceAssignmentExpression.class);
+					expression = getDiagramEditor().getModelHandler().createStandby(
+							 resourceAssignment, PACKAGE.getResourceAssignmentExpression_Expression(),
+							 FormalExpression.class);
 				}
 				else if (task.getResources().get(0) instanceof PotentialOwner){
 					owner = (PotentialOwner)task.getResources().get(0);
+					resourceAssignment = owner.getResourceAssignmentExpression();
+					expression = (FormalExpression)resourceAssignment.getExpression();
 				}
-				TextObjectEditor editor = new TextObjectEditor(this, owner, PACKAGE.getResourceRole_Name());
+				TextObjectEditor editor = new TextObjectEditor(this, expression, PACKAGE.getFormalExpression_Body());
 				editor.createControl(getAttributesParent(), "Actor", SWT.NONE);
 			}
 		}
