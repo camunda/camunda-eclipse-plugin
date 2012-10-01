708342a519e96479ba4cd1093e6fa7cb4f2cbde7
Nico Rehwaldt <git_nikku@nixis.de>
2012-10-01 11:54:34 +0200

    Exception in test is properly propagated

--- org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/util/CommandRule.java
+++ org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/util/CommandRule.java
@@ -1,5 +1,7 @@
 package org.eclipse.bpmn2.modeler.core.test.util;
 
+import java.lang.reflect.Method;
+
 import org.eclipse.bpmn2.modeler.core.test.importer.AbstractImportBpmn2ModelTest;
 import org.eclipse.bpmn2.modeler.core.test.importer.AbstractTestCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
@@ -19,41 +21,72 @@ public class CommandRule implements MethodRule {
 		if (resource != null && target instanceof AbstractImportBpmn2ModelTest) {
 			final AbstractImportBpmn2ModelTest testCase = (AbstractImportBpmn2ModelTest) target;
 			
-			String resourceUrl = resource.value();
-			if (resourceUrl == null || resourceUrl.isEmpty()) {
-				resourceUrl = target.getClass().getName().replaceAll("\\.", "/") + "." + method.getName() + ".bpmn";
-			}
+			String resourceUrl = getResourceUrl(resource, target, method.getMethod());
 			
-			final String finalResourceUrl = resourceUrl;
-			
-			return new Statement() {
-				
-				@Override
-				public void evaluate() throws Throwable {
-					try {
-						TransactionalEditingDomain domain = testCase.createEditingDomain(finalResourceUrl);
-						
-						domain.getCommandStack().execute(new AbstractTestCommand(testCase, "test.bpmn") {
-							
-							@Override
-							public void test(IDiagramTypeProvider diagramTypeProvider, Diagram diagram) {
-								testCase.setDiagramTypeProvider(diagramTypeProvider);
-								testCase.setDiagram(diagram);
-								
-								try {
-									base.evaluate();
-								} catch (Throwable e) {
-									throw new RuntimeException(e);
-								}
-							}
-						});
-					} finally {
-						testCase.disposeEditingDomain();
-					}
-				}
-			};
+			return new StatementExtension(testCase, resourceUrl, base);
 		} else {
 			return base;
+		}
+	}
+	
+	private String getResourceUrl(DiagramResource resource, Object o, Method m) {
+		String url = resource.value();
+		if (url == null || url.isEmpty()) {
+			url = o.getClass().getName().replaceAll("\\.", "/") + "." + m.getName() + ".bpmn";
+		}
+		
+		return url;
+	}
+
+	private class StatementExtension extends Statement {
+		
+		private final AbstractImportBpmn2ModelTest testCase;
+		private final String resourceUrl;
+		private final Statement base;
+
+		private Throwable thrownException;
+		
+		private StatementExtension(AbstractImportBpmn2ModelTest testCase, String resourceUrl, Statement base) {
+			this.testCase = testCase;
+			this.resourceUrl = resourceUrl;
+			this.base = base;
+		}
+
+		@Override
+		public void evaluate() throws Throwable {
+			try {
+				TransactionalEditingDomain domain = testCase.createEditingDomain(resourceUrl);
+				
+				domain.getCommandStack().execute(new AbstractTestCommand(testCase, "test.bpmn") {
+					
+					@Override
+					public void test(IDiagramTypeProvider diagramTypeProvider, Diagram diagram) {
+						testCase.setDiagramTypeProvider(diagramTypeProvider);
+						testCase.setDiagram(diagram);
+						
+						try {
+							base.evaluate();
+						} catch (Throwable e) {
+							setThrownException(e);
+						}
+					}
+				});
+				
+				Throwable e = getThrownException();
+				if (e != null) {
+					throw e;
+				}
+			} finally {
+				testCase.disposeEditingDomain();
+			}
+		}
+		
+		public void setThrownException(Throwable thrownException) {
+			this.thrownException = thrownException;
+		}
+		
+		public Throwable getThrownException() {
+			return thrownException;
 		}
 	}
 }
