5c42e03931fc5c05efe9e096e8dc2d2beef0b77a
romansmirnov <roman.smirnov@camunda.com>
2012-11-14 14:41:46 +0100

    HEMERA-3133: Add testcase to test the vertical and horizontal move of a task

--- org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/feature/move/MoveFlowNodeFeatureTest.java
+++ org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/feature/move/MoveFlowNodeFeatureTest.java
@@ -26,13 +26,13 @@ import org.junit.Test;
  * @author Nico Rehwaldt
  */
 public class MoveFlowNodeFeatureTest extends AbstractFeatureTest {
-	
+
 	@Test
 	@DiagramResource
 	public void testMoveGatewayVerticalLayout() {
 		Shape gatewayShape = ShapeUtil.findShapeByBusinessObjectId(diagram, "ExclusiveGateway_1");
 		ContainerShape laneShape = (ContainerShape) ShapeUtil.findShapeByBusinessObjectId(diagram, "Lane_1");
-		
+
 		move(gatewayShape, diagramTypeProvider)
 		.by(0 , -85)
 		.toContainer(laneShape)
@@ -84,11 +84,11 @@ public class MoveFlowNodeFeatureTest extends AbstractFeatureTest {
 		assertEquals(0, ((FixPointAnchor) seq7Connection.getEnd()).getLocation().getX());
 		assertEquals(25, ((FixPointAnchor) seq7Connection.getEnd()).getLocation().getY());
 	}
-	
+
 	@Test
 	@DiagramResource
 	public void testMoveShapeOutOfContainer() {
-		
+
 		// find shapes
 		Shape userTaskShape = ShapeUtil.findShapeByBusinessObjectId(diagram, "UserTask_1");
 		ContainerShape subProcessShape = (ContainerShape) ShapeUtil.findShapeByBusinessObjectId(diagram, "SubProcess_1");
@@ -104,18 +104,18 @@ public class MoveFlowNodeFeatureTest extends AbstractFeatureTest {
 		move(userTaskShape, diagramTypeProvider)
 			.toContainer(processShape)
 			.execute();
-		
+
 		// now the usertask is contained in the process
 		assertThat(subProcessShape).doesNotHaveChild(userTaskShape);
 		assertThat(processShape).hasChild(userTaskShape);
 		assertThat(userTaskShape).hasParentModelElement(subProcessElement.eContainer());
-		
+
 	}
-	
+
 	@Test
 	@DiagramResource
 	public void testMoveShapeIntoContainer() {
-		
+
 		// find shapes
 		Shape userTaskShape = ShapeUtil.findShapeByBusinessObjectId(diagram, "UserTask_1");
 		ContainerShape subProcessShape = (ContainerShape) ShapeUtil.findShapeByBusinessObjectId(diagram, "SubProcess_1");
@@ -131,70 +131,69 @@ public class MoveFlowNodeFeatureTest extends AbstractFeatureTest {
 		move(userTaskShape, diagramTypeProvider)
 			.toContainer(subProcessShape)
 			.execute();
-		
+
 		// now the usertask is contained in the subprocess
 		assertThat(subProcessShape).hasChild(userTaskShape);
-		assertThat(processShape).doesNotHaveChild(userTaskShape);		
+		assertThat(processShape).doesNotHaveChild(userTaskShape);
 		assertThat(userTaskShape).hasParentModelElement(subProcessElement);
-		
+
 	}
 
 	@Test
 	@DiagramResource
 	public void testMoveTaskVerticalLayout() {
-	   Shape taskShape = ShapeUtil.findShapeByBusinessObjectId(diagram, "Task_1");
-	   ContainerShape participantShape = (ContainerShape) ShapeUtil.findShapeByBusinessObjectId(diagram, "Participant_1");
-	   
-	   FreeFormConnection seq1Connection = (FreeFormConnection) ShapeUtil.findConnectionByBusinessObjectId(diagram, "SequenceFlow_1");
-	   FreeFormConnection seq2Connection = (FreeFormConnection) ShapeUtil.findConnectionByBusinessObjectId(diagram, "SequenceFlow_2");
-	    
-	   assertThat(seq1Connection.getBendpoints()).isEmpty();
-	   assertThat(seq2Connection.getBendpoints()).isEmpty();
-	   
-	   move(taskShape, diagramTypeProvider)
-	    .by(0 , 180)
-	    .toContainer(participantShape)
-	    .execute();
+		Shape taskShape = ShapeUtil.findShapeByBusinessObjectId(diagram, "Task_1");
+		ContainerShape participantShape = (ContainerShape) ShapeUtil.findShapeByBusinessObjectId(diagram, "Participant_1");
 
-	   
-	   IPeLayoutService peLayout = Graphiti.getPeLayoutService();
-	   
-	   ILocation seq1StartLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getStart());
-	   assertThat(seq1StartLoc.getX()).isEqualTo(216);
-	   assertThat(seq1StartLoc.getY()).isEqualTo(165);
-	   
-	   ILocation seq1EndLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getEnd());
-     assertThat(seq1EndLoc.getX()).isEqualTo(320);
-     assertThat(seq1EndLoc.getY()).isEqualTo(345);
-	   
-     assertThat(seq1Connection.getBendpoints().size()).isEqualTo(2);
-     
-     Point seq1FirstPoint = seq1Connection.getBendpoints().get(0);
-     assertThat(seq1FirstPoint.getX()).isEqualTo(372);
-     assertThat(seq1FirstPoint.getY()).isEqualTo(165);
-     
-     Point seq1SecondPoint = seq1Connection.getBendpoints().get(1);
-     assertThat(seq1SecondPoint.getX()).isEqualTo(372);
-     assertThat(seq1SecondPoint.getY()).isEqualTo(345);     
-     
-     ILocation seq2StartLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getStart());
-     assertThat(seq2StartLoc.getX()).isEqualTo(430);
-     assertThat(seq2StartLoc.getY()).isEqualTo(345);
-     
-     ILocation seq2EndLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getEnd());
-     assertThat(seq2EndLoc.getX()).isEqualTo(534);
-     assertThat(seq2EndLoc.getY()).isEqualTo(165);
-     
-     assertThat(seq2Connection.getBendpoints().size()).isEqualTo(2);
-     
-     Point seq2FirstPoint = seq2Connection.getBendpoints().get(0);
-     assertThat(seq2FirstPoint.getX()).isEqualTo(482);
-     assertThat(seq2FirstPoint.getY()).isEqualTo(345);
-     
-     Point seq2SecondPoint = seq2Connection.getBendpoints().get(1);
-     assertThat(seq2SecondPoint.getX()).isEqualTo(482);
-     assertThat(seq2SecondPoint.getY()).isEqualTo(165);     
-     
+		FreeFormConnection seq1Connection = (FreeFormConnection) ShapeUtil.findConnectionByBusinessObjectId(diagram, "SequenceFlow_1");
+		FreeFormConnection seq2Connection = (FreeFormConnection) ShapeUtil.findConnectionByBusinessObjectId(diagram, "SequenceFlow_2");
+
+		assertThat(seq1Connection.getBendpoints()).isEmpty();
+		assertThat(seq2Connection.getBendpoints()).isEmpty();
+
+		move(taskShape, diagramTypeProvider)
+			.by(0, 180)
+			.toContainer(participantShape)
+			.execute();
+
+		IPeLayoutService peLayout = Graphiti.getPeLayoutService();
+
+		ILocation seq1StartLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getStart());
+		assertThat(seq1StartLoc.getX()).isEqualTo(216);
+		assertThat(seq1StartLoc.getY()).isEqualTo(165);
+
+		ILocation seq1EndLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getEnd());
+		assertThat(seq1EndLoc.getX()).isEqualTo(320);
+		assertThat(seq1EndLoc.getY()).isEqualTo(345);
+
+		assertThat(seq1Connection.getBendpoints().size()).isEqualTo(2);
+
+		Point seq1FirstPoint = seq1Connection.getBendpoints().get(0);
+		assertThat(seq1FirstPoint.getX()).isEqualTo(372);
+		assertThat(seq1FirstPoint.getY()).isEqualTo(165);
+
+		Point seq1SecondPoint = seq1Connection.getBendpoints().get(1);
+		assertThat(seq1SecondPoint.getX()).isEqualTo(372);
+		assertThat(seq1SecondPoint.getY()).isEqualTo(345);
+
+		ILocation seq2StartLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getStart());
+		assertThat(seq2StartLoc.getX()).isEqualTo(430);
+		assertThat(seq2StartLoc.getY()).isEqualTo(345);
+
+		ILocation seq2EndLoc = peLayout.getLocationRelativeToDiagram(seq1Connection.getEnd());
+		assertThat(seq2EndLoc.getX()).isEqualTo(534);
+		assertThat(seq2EndLoc.getY()).isEqualTo(165);
+
+		assertThat(seq2Connection.getBendpoints().size()).isEqualTo(2);
+
+		Point seq2FirstPoint = seq2Connection.getBendpoints().get(0);
+		assertThat(seq2FirstPoint.getX()).isEqualTo(482);
+		assertThat(seq2FirstPoint.getY()).isEqualTo(345);
+
+		Point seq2SecondPoint = seq2Connection.getBendpoints().get(1);
+		assertThat(seq2SecondPoint.getX()).isEqualTo(482);
+		assertThat(seq2SecondPoint.getY()).isEqualTo(165);
+
 	}
 
 	@Test
@@ -222,4 +221,60 @@ public class MoveFlowNodeFeatureTest extends AbstractFeatureTest {
 	private Point point(int x, int y) {
 		return Graphiti.getGaService().createPoint(x, y);
 	}
+	@Test
+	@DiagramResource
+	public void testMoveTaskVerticalAndHorizontalLayout() {
+		Shape taskShape = ShapeUtil.findShapeByBusinessObjectId(diagram, "Task_2");
+		ContainerShape participantShape = (ContainerShape) ShapeUtil.findShapeByBusinessObjectId(diagram, "Participant_1");
+
+		FreeFormConnection seq2Connection = (FreeFormConnection) ShapeUtil.findConnectionByBusinessObjectId(diagram, "SequenceFlow_2");
+		FreeFormConnection seq3Connection = (FreeFormConnection) ShapeUtil.findConnectionByBusinessObjectId(diagram, "SequenceFlow_3");
+
+		assertThat(seq2Connection.getBendpoints()).isEmpty();
+		assertThat(seq3Connection.getBendpoints()).isEmpty();
+
+		move(taskShape, diagramTypeProvider)
+			.by(-105, -95)
+			.toContainer(participantShape)
+			.execute();
+
+		IPeLayoutService peLayout = Graphiti.getPeLayoutService();
+
+		ILocation seq2StartLoc = peLayout.getLocationRelativeToDiagram(seq2Connection.getStart());
+		assertThat(seq2StartLoc.getX()).isEqualTo(313);
+		assertThat(seq2StartLoc.getY()).isEqualTo(325);
+
+		ILocation seq2EndLoc = peLayout.getLocationRelativeToDiagram(seq2Connection.getEnd());
+		assertThat(seq2EndLoc.getX()).isEqualTo(368);
+		assertThat(seq2EndLoc.getY()).isEqualTo(280);
+
+		assertThat(seq2Connection.getBendpoints().size()).isEqualTo(2);
+
+		Point seq2FirstPoint = seq2Connection.getBendpoints().get(0);
+		assertThat(seq2FirstPoint.getX()).isEqualTo(313);
+		assertThat(seq2FirstPoint.getY()).isEqualTo(302);
+
+		Point seq2SecondPoint = seq2Connection.getBendpoints().get(1);
+		assertThat(seq2SecondPoint.getX()).isEqualTo(368);
+		assertThat(seq2SecondPoint.getY()).isEqualTo(302);
+
+		ILocation seq3StartLoc = peLayout.getLocationRelativeToDiagram(seq3Connection.getStart());
+		assertThat(seq3StartLoc.getX()).isEqualTo(423);
+		assertThat(seq3StartLoc.getY()).isEqualTo(255);
+
+		ILocation seq3EndLoc = peLayout.getLocationRelativeToDiagram(seq3Connection.getEnd());
+		assertThat(seq3EndLoc.getX()).isEqualTo(578);
+		assertThat(seq3EndLoc.getY()).isEqualTo(350);
+
+		assertThat(seq3Connection.getBendpoints().size()).isEqualTo(2);
+
+		Point seq3FirstPoint = seq3Connection.getBendpoints().get(0);
+		assertThat(seq3FirstPoint.getX()).isEqualTo(500);
+		assertThat(seq3FirstPoint.getY()).isEqualTo(255);
+
+		Point seq3SecondPoint = seq3Connection.getBendpoints().get(1);
+		assertThat(seq3SecondPoint.getX()).isEqualTo(500);
+		assertThat(seq3SecondPoint.getY()).isEqualTo(350);
+	}
+
 }
