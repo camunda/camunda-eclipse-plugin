569dddfcbaa2ef180d33d2a516e0491ff3ef5aac
Andreas Drobisch <andreas.drobisch@camunda.com>
2012-12-07 18:02:06 +0100

    failing test for participant delete feature

---
+++ org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/feature/delete/DeleteParticipantFeatureTest.java
@@ -0,0 +1,18 @@
+package org.eclipse.bpmn2.modeler.core.test.feature.delete;
+
+import static org.junit.Assert.fail;
+
+import org.eclipse.bpmn2.modeler.core.test.feature.AbstractFeatureTest;
+import org.eclipse.bpmn2.modeler.ui.features.participant.DeleteParticipantFeature;
+import org.junit.Test;
+
+/**
+ * @author adrobisch
+ */
+public class DeleteParticipantFeatureTest extends AbstractFeatureTest {
+	
+	@Test
+	public void testCreateParticipant() {
+		fail(""+DeleteParticipantFeature.class+"must be tested");
+	}
+}
