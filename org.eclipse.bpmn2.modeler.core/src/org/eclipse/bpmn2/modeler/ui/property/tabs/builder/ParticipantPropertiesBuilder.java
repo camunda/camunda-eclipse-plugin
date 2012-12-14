136d7e712eb8e936e4d5bc51200c5debdcbe05e6
Andreas Drobisch <andreas.drobisch@camunda.com>
2012-12-14 14:54:22 +0100

    HEMERA-3248 make isExecutable editable for in participant properties

--- org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/builder/ParticipantPropertiesBuilder.java
+++ org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/builder/ParticipantPropertiesBuilder.java
@@ -1,7 +1,9 @@
 package org.eclipse.bpmn2.modeler.ui.property.tabs.builder;
 
+import org.eclipse.bpmn2.Bpmn2Package;
 import org.eclipse.bpmn2.Participant;
 import org.eclipse.bpmn2.Process;
+import org.eclipse.bpmn2.modeler.ui.property.tabs.util.PropertyUtil;
 import org.eclipse.graphiti.ui.platform.GFPropertySection;
 import org.eclipse.swt.widgets.Composite;
 
@@ -22,5 +24,8 @@ public class ParticipantPropertiesBuilder extends AbstractPropertiesBuilder<Part
 
 		new NamePropertyBuilder(parent, section, process, "Process Name").create();
 		new IdPropertyBuilder(parent, section, process, "Process Id").create();
+		
+		PropertyUtil.createCheckbox(section, parent, "Is Executable",
+				Bpmn2Package.eINSTANCE.getProcess_IsExecutable(), process);
 	}
 }
