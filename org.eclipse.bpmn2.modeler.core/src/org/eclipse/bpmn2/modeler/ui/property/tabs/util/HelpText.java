da1111f103c5c1c67a032718b2204f2676d184ea
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2012-12-20 15:16:02 +0100

    Fix of help text

--- org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/util/HelpText.java
+++ org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/util/HelpText.java
@@ -11,6 +11,6 @@ public class HelpText {
 	public static final String TABLE_HELP = "Add or remove table entry by right-click";
 	
 	public static final String TIME_DATE = "Enter date in ISO 8601 format (e.g. \"2011-03-11T12:13:14\")";
-	public static final String TIME_DURATION = "Enter dureation in ISO 8601 format (e.g. \"P4DT12H30M5S\" represents a duration of \"four days, twelve hours, thirty minutes, and five seconds\")";
+	public static final String TIME_DURATION = "Enter duration in ISO 8601 format (e.g. \"P4DT12H30M5S\" represents a duration of \"four days, twelve hours, thirty minutes, and five seconds\")";
 	public static final String TIME_CYCLE = "Enter repeating interval in ISO 8601 format (e.g. \"R3/PT10M\" represents \"3 repeating intervals, each lasting 10 minutes\")";
 }
