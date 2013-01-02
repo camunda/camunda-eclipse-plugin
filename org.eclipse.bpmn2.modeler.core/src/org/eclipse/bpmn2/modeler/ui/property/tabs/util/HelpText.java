c566f48782cf27cda800b4ab487e57a27d0c2e6d
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-01-02 16:07:28 +0100

    HEMERA-3368: Shortened help texts

--- org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/util/HelpText.java
+++ org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/util/HelpText.java
@@ -10,7 +10,7 @@ public class HelpText {
 
 	public static final String TABLE_HELP = "Add or remove table entry by right-click";
 	
-	public static final String TIME_DATE = "Enter date in ISO 8601 format (e.g. \"2011-03-11T12:13:14\")";
-	public static final String TIME_DURATION = "Enter duration in ISO 8601 format (e.g. \"P4DT12H30M5S\" represents a duration of \"four days, twelve hours, thirty minutes, and five seconds\")";
-	public static final String TIME_CYCLE = "Enter repeating interval in ISO 8601 format (e.g. \"R3/PT10M\" represents \"3 repeating intervals, each lasting 10 minutes\")";
+	public static final String TIME_DATE = "Date in ISO 8601 format (e.g. \"2011-03-11T12:13:14\")";
+	public static final String TIME_DURATION = "Duration in ISO 8601 format (e.g. \"P4DT12H30M5S\" as a duration of \"four days, 12 hours, 30 minutes, and five seconds\")";
+	public static final String TIME_CYCLE = "Retry interval in ISO 8601 format (e.g. \"R3/PT10M\" for \"3 cycles, every 10 minutes\")";
 }
