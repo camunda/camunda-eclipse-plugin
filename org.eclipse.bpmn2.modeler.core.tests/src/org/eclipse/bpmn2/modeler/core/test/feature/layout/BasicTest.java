517d74fe9bd896c2c93acc5c826d5844fbd894b4
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-01-08 14:03:01 +0100

    HEMERA-3417: Test cases

--- org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/feature/layout/BasicTest.java
+++ org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/feature/layout/BasicTest.java
@@ -1,8 +1,14 @@
 package org.eclipse.bpmn2.modeler.core.test.feature.layout;
 
+import static org.eclipse.bpmn2.modeler.core.layout.util.ConversionUtil.point;
+import static org.eclipse.bpmn2.modeler.core.test.util.assertions.Bpmn2ModelAssertions.assertThat;
+
+import org.eclipse.bpmn2.modeler.core.layout.util.LayoutUtil;
 import org.eclipse.bpmn2.modeler.core.test.feature.AbstractFeatureTest;
 import org.eclipse.bpmn2.modeler.core.test.util.DiagramResource;
 import org.eclipse.bpmn2.modeler.core.test.util.Util;
+import org.eclipse.bpmn2.modeler.core.test.util.operations.MoveFlowElementOperation;
+import org.eclipse.graphiti.datatypes.IRectangle;
 import org.eclipse.graphiti.mm.pictograms.FreeFormConnection;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.junit.Test;
@@ -21,11 +27,36 @@ public class BasicTest extends AbstractFeatureTest {
 		FreeFormConnection sequenceFlow5 = (FreeFormConnection) Util.findConnectionByBusinessObjectId(diagram, "SequenceFlow_5");
 		
 		Shape taskShape = Util.findShapeByBusinessObjectId(diagram, "Task_6");
+		
+		// when moving shape above connected shape
+		MoveFlowElementOperation.move(taskShape, getDiagramTypeProvider())
+			.by(130, -91)
+			.execute();
+		
+		// make sure we can do this (no exception thrown)
+		
+		IRectangle rect = LayoutUtil.getAbsoluteRectangle(taskShape);
+		
+		// then movement was executed
+		assertThat(point(rect)).isEqualTo(point(360, 289));
 	}
 	
 	@Test
 	@DiagramResource("org/eclipse/bpmn2/modeler/core/test/feature/layout/BoundaryEventTest.testBase.bpmn")
 	public void testLayoutOverlappingAnchorPoints() {
+			
+		Shape taskShape = Util.findShapeByBusinessObjectId(diagram, "Task_6");
 		
+		// when moving both anchor points above each other
+		MoveFlowElementOperation.move(taskShape, getDiagramTypeProvider())
+			.by(110, -130)
+			.execute();
+		
+		// make sure we can do this (no exception thrown)
+		
+		IRectangle rect = LayoutUtil.getAbsoluteRectangle(taskShape);
+		
+		// then movement was executed
+		assertThat(point(rect)).isEqualTo(point(340, 250));
 	}
 }
