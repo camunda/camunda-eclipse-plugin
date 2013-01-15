c9941dda19fe2e6323e36257c802c92b28cb26b1
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-01-15 15:10:07 +0100

    HEMERA-3524: Test case for t+b move

---
+++ org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/feature/add/AddPoolFeatureTest.java
@@ -0,0 +1,15 @@
+package org.eclipse.bpmn2.modeler.core.test.feature.add;
+
+import org.eclipse.bpmn2.modeler.core.test.feature.AbstractFeatureTest;
+import org.eclipse.bpmn2.modeler.core.test.util.DiagramResource;
+import org.junit.Test;
+
+public class AddPoolFeatureTest extends AbstractFeatureTest {
+
+	@Test
+	@DiagramResource("org/eclipse/bpmn2/modeler/core/test/feature/move/MoveFlowNodeFeature.testTaskWithBoundaryEvent.bpmn")
+	public void testAddToNonEmptyProcessWithBoundaryEvent() throws Exception {
+		
+		
+	}
+}
