9fcfb02b2dc7a388597a301a6440ec99894dc516
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-01-21 14:13:49 +0100

    HEMERA-3418: Add help text

--- org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/util/HelpText.java
+++ org.eclipse.bpmn2.modeler.core/src/org/eclipse/bpmn2/modeler/ui/property/tabs/util/HelpText.java
@@ -15,4 +15,12 @@ public class HelpText {
 	public static final String TIME_CYCLE = "Retry interval in ISO 8601 format (e.g. \"R3/PT10M\" for \"3 cycles, every 10 minutes\")";
 
 	public static final String ELEMENT_DEF_TABLE = "%ss can be defined below";
+
+	private static final String ASYNC_LINK_TO_USER_GUIDE = 
+			"https://app.camunda.com/confluence/display/foxUserGuide/Thread+and+Transaction+Management#ThreadandTransactionManagement-AsynchronousContinuation";
+	
+	
+	public static final String ASYNC_FLAG = String.format(
+			"More infomation on asynchronous continuation can be found in the <a href=\"%s\">user guide</a>.", ASYNC_LINK_TO_USER_GUIDE);
+	
 }
