d86b0c4f5ca56640495b5c1b666fbbb6ea9819a8
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-01-31 13:33:48 +0100

    DataStore / DataObject labels read from DI (HEMERA-3716)

--- org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/importer/dataitems/ImportDatastoreTest.java
+++ org.eclipse.bpmn2.modeler.core.tests/src/org/eclipse/bpmn2/modeler/core/test/importer/dataitems/ImportDatastoreTest.java
@@ -1,13 +1,17 @@
 package org.eclipse.bpmn2.modeler.core.test.importer.dataitems;
 
 import static org.fest.assertions.api.Assertions.assertThat;
+import static org.eclipse.bpmn2.modeler.core.layout.util.ConversionUtil.rectangle;
 
 import org.eclipse.bpmn2.DataStoreReference;
 import org.eclipse.bpmn2.modeler.core.importer.ModelImport;
+import org.eclipse.bpmn2.modeler.core.layout.util.LayoutUtil;
 import org.eclipse.bpmn2.modeler.core.test.importer.AbstractImportBpmnModelTest;
 import org.eclipse.bpmn2.modeler.core.test.util.DiagramResource;
+import org.eclipse.bpmn2.modeler.core.test.util.Util;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.EObject;
+import org.eclipse.graphiti.datatypes.IRectangle;
 import org.eclipse.graphiti.mm.pictograms.PictogramLink;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.junit.Assert;
@@ -35,6 +39,23 @@ public class ImportDatastoreTest extends AbstractImportBpmnModelTest {
 		
 		assertThat(businessObject).isInstanceOf(DataStoreReference.class);
 		assertThat(((DataStoreReference)businessObject).getId()).isEqualTo("DataStoreRef_1");
+	}
+
+	@Test
+	@DiagramResource("org/eclipse/bpmn2/modeler/core/test/importer/dataitems/ImportDatastoreTest.testImportDatastore.bpmn")
+	public void testImportDatastoreShouldGetBoundsFromDi() {
+		
+		// given 
+		IRectangle expectedBounds = rectangle(310, 210, 50, 50);
+
+		// when
+		ModelImport importer = createModelImport();
+		importer.execute();
+		
+		Shape dataStoreShape = Util.findShapeByBusinessObjectId(diagram, "DataStoreRef_1");
+		IRectangle dataStoreBounds = LayoutUtil.getAbsoluteBounds(dataStoreShape);
+		
+		assertThat(dataStoreBounds).isEqualTo(expectedBounds);
 	}
 	
 	@Test
