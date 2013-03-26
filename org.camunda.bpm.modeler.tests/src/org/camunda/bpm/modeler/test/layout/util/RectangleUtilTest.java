83f68c2ccea795e84e9c5b79e1af57054745d4fe
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-03-26 14:43:21 +0100

    feat(layouting): add diff utility for rectangles

--- org.camunda.bpm.modeler.tests/src/org/camunda/bpm/modeler/test/layout/util/RectangleUtilTest.java
+++ org.camunda.bpm.modeler.tests/src/org/camunda/bpm/modeler/test/layout/util/RectangleUtilTest.java
@@ -5,6 +5,7 @@ import static org.camunda.bpm.modeler.core.layout.util.RectangleUtil.*;
 import static org.camunda.bpm.modeler.test.util.assertions.Bpmn2ModelAssertions.assertThat;
 import static org.fest.assertions.api.Assertions.assertThat;
 
+import org.camunda.bpm.modeler.core.layout.util.RectangleUtil.ResizeDiff;
 import org.camunda.bpm.modeler.core.layout.util.VectorUtil;
 import org.camunda.bpm.modeler.core.layout.util.LayoutUtil.Sector;
 import org.eclipse.draw2d.geometry.Vector;
@@ -133,5 +134,51 @@ public class RectangleUtilTest {
 		// translate by point
 		assertThat(translate(rect, point(-10, -10))).isEqualTo(rectangle(0, 0, 40, 40));
 		assertThat(translate(rect, point(10, 10))).isEqualTo(rectangle(20, 20, 40, 40));
+	}
+
+	@Test
+	public void testSubstract() {
+		
+		// given
+		IRectangle rect = rectangle(10, 10, 40, 40);
+		
+		// check correct translation
+		
+		// translate by rect
+		assertThat(substract(rect, rect)).isEqualTo(rectangle(0, 0, 0, 0));
+		assertThat(substract(rect, rectangle(10, 10, 10, 10))).isEqualTo(rectangle(0, 0, 30, 30));
+	}
+
+	@Test
+	public void testResizeDiff() {
+		
+		// given
+		IRectangle rect = rectangle(10, 10, 40, 40);
+		
+		// check correct resize diff computation
+
+		// top right
+		ResizeDiff diff1 = resizeDiff(rect, rectangle(10, 0, 50, 50));
+		
+		assertThat(diff1.getResizeDelta()).isEqualTo(point(10, -10));
+		assertThat(diff1.getResizeDirection()).isEqualTo(Sector.TOP_RIGHT);
+		
+		// top left
+		ResizeDiff diff2 = resizeDiff(rect, rectangle(0, 0, 50, 50));
+		
+		assertThat(diff2.getResizeDelta()).isEqualTo(point(-10, -10));
+		assertThat(diff2.getResizeDirection()).isEqualTo(Sector.TOP_LEFT);
+		
+		// bottom right
+		ResizeDiff diff3 = resizeDiff(rect, rectangle(10, 10, 50, 50));
+		
+		assertThat(diff3.getResizeDelta()).isEqualTo(point(10, 10));
+		assertThat(diff3.getResizeDirection()).isEqualTo(Sector.BOTTOM_RIGHT);
+		
+		// bottom left
+		ResizeDiff diff0 = resizeDiff(rect, rectangle(0, 10, 50, 50));
+		
+		assertThat(diff0.getResizeDelta()).isEqualTo(point(-10, 10));
+		assertThat(diff0.getResizeDirection()).isEqualTo(Sector.BOTTOM_LEFT);
 	}
 }
--- org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/core/layout/util/RectangleUtil.java
+++ org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/core/layout/util/RectangleUtil.java
@@ -1,17 +1,52 @@
 package org.camunda.bpm.modeler.core.layout.util;
 
 import static org.camunda.bpm.modeler.core.layout.util.ConversionUtil.rectangle;
+import static org.camunda.bpm.modeler.core.layout.util.ConversionUtil.point;
 
 import org.camunda.bpm.modeler.core.layout.util.LayoutUtil.Sector;
 import org.eclipse.graphiti.datatypes.IRectangle;
 import org.eclipse.graphiti.mm.algorithms.styles.Point;
 
 /**
- * Common operations on rectangles.
+ * Common operations on rectangles
  * 
  * @author nico.rehwaldt
  */
 public class RectangleUtil {
+
+	/**
+	 * Represents diff between two rects which resulted during a resize operation
+	 * 
+	 * @author nico.rehwaldt
+	 */
+	public static class ResizeDiff {
+		
+		private Point delta;
+		private Sector direction;
+		
+		public ResizeDiff(Point delta, Sector direction) {
+			this.delta = delta;
+			this.direction = direction;
+		}
+		
+		/**
+		 * Returns the resize delta of this diff
+		 * 
+		 * @return
+		 */
+		public Point getResizeDelta() {
+			return delta;
+		}
+		
+		/**
+		 * Returns the resize sector of this diff
+		 * 
+		 * @return
+		 */
+		public Sector getResizeDirection() {
+			return direction;
+		}
+	}
 
 	public static IRectangle resize(IRectangle rect, Point delta, Sector direction) {
 		
@@ -58,6 +93,68 @@ public class RectangleUtil {
 			rect.getY() + translation.getY(),
 			rect.getWidth() + translation.getWidth(), 
 			rect.getHeight() + translation.getHeight());
+	}
+	
+	/**
+	 * Substract the given rectangle from other rect
+	 * 
+	 * @param rect
+	 * @param substractRect
+	 * 
+	 * @return
+	 */
+	public static IRectangle substract(IRectangle rect, IRectangle substractRect) {
+		IRectangle substraction = rectangle(
+			-1 * substractRect.getX(),
+			-1 * substractRect.getY(),
+			-1 * substractRect.getWidth(),
+			-1 * substractRect.getHeight());
+		
+		return translate(rect, substraction);
+	}
+	
+	/**
+	 * Returns the {@link ResizeDiff}, ie. the delta between two rects as 
+	 * a result of a resize operation. 
+	 * 
+	 * @param preResizeRect
+	 * @param postResizeRect
+	 * 
+	 * @return
+	 * 
+	 * @throws IllegalArgumentException if the resize diff cannot be deduced from the shapes
+	 */
+	public static ResizeDiff resizeDiff(IRectangle preResizeRect, IRectangle postResizeRect) {
+		
+		IRectangle diff = substract(postResizeRect, preResizeRect);
+		
+		int dx = 0, dy = 0;
+		
+		boolean left = false, right = false, top = false, bottom = false;
+		
+		if (diff.getX() != 0) {
+			dx = diff.getX();
+			left = true;
+		}
+		
+		if (diff.getY() != 0) {
+			dy = diff.getY();
+			top = true;
+		}
+		
+		if (!left && diff.getWidth() != 0) {
+			dx = diff.getWidth();
+			right = true;
+		}
+		
+		if (!top && diff.getHeight() != 0) {
+			dy = diff.getHeight();
+			bottom = true;
+		}
+		
+		Sector direction = Sector.fromBooleans(left, right, top, bottom);
+		
+		return new ResizeDiff(point(dx, dy), direction);
 	}
 	
 	/**
