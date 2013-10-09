0509a62b66acbb3087849466deb59c20bf587d45
romansmirnov <roman.smirnov@camunda.com>
2013-10-09 14:24:07 +0200

    fix(attribute/xsiType): Prevent xsi:type attribute
    - prevents that a 'xsi:type="xsd:anyType"' will be added to an element,
      because the element has the type "AnyType"
    
    related to CAM-1341

--- org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/runtime/engine/model/util/ModelResourceImpl.java
+++ org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/runtime/engine/model/util/ModelResourceImpl.java
@@ -55,9 +55,11 @@ import org.eclipse.emf.ecore.xmi.XMLResource;
 import org.eclipse.emf.ecore.xmi.XMLSave;
 import org.eclipse.emf.ecore.xmi.impl.XMLLoadImpl;
 import org.eclipse.emf.ecore.xmi.impl.XMLString;
+import org.eclipse.emf.ecore.xml.type.AnyType;
 import org.eclipse.emf.transaction.RecordingCommand;
 import org.eclipse.emf.transaction.TransactionalEditingDomain;
 import org.eclipse.emf.transaction.util.TransactionUtil;
+import org.omg.CORBA.Any;
 import org.xml.sax.helpers.DefaultHandler;
 
 /**
@@ -111,9 +113,14 @@ public class ModelResourceImpl extends Bpmn2ModelerResourceImpl {
 			private boolean needTargetNamespace = true;
 			
 			@Override
-			protected void saveTypeAttribute(EClass eClass) {			  
+			protected void saveTypeAttribute(EClass eClass) {
 				if (!eClass.getEPackage().getNsPrefix().equals("activiti") &&
-				    !eClass.getEPackage().getNsPrefix().equals("camunda")){
+				    !eClass.getEPackage().getNsPrefix().equals("camunda") &&
+				    
+				    // prevent that 'xsi:type="xsd:anyType"' will be added to 
+				    // an element which type is "AnyTypeImpl"
+				    !eClass.getEPackage().getNsPrefix().equals("ecore.xml.type")) {
+					
 					super.saveTypeAttribute(eClass);
 				}
 			}
