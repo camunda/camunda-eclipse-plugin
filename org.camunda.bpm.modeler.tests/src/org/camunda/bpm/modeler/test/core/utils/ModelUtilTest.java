b652ca10526ddcb4de14e81d95c2bdb176e78318
unknown <kristin.polenz@camunda.com>
2013-10-25 09:23:27 +0200

    feat(compensateThrowingEvent/prop): Add event tab fields for intermediate throwing compensation event.
    - it is possible to configure intermediate throwing compensation event on
      event tab
    - set default value "true" for "wait for completion" attribute
    - add test cases for method getFlowElementsContainer
    
    related to #CAM-1392

--- org.camunda.bpm.modeler.tests/src/org/camunda/bpm/modeler/test/core/utils/ModelUtilTest.java
+++ org.camunda.bpm.modeler.tests/src/org/camunda/bpm/modeler/test/core/utils/ModelUtilTest.java
@@ -8,7 +8,9 @@ import org.camunda.bpm.modeler.core.utils.ModelUtil;
 import org.camunda.bpm.modeler.test.feature.AbstractNonTransactionalFeatureTest;
 import org.camunda.bpm.modeler.test.util.DiagramResource;
 import org.camunda.bpm.modeler.test.util.Util;
+import org.eclipse.bpmn2.BaseElement;
 import org.eclipse.bpmn2.DataObject;
+import org.eclipse.bpmn2.FlowElementsContainer;
 import org.eclipse.emf.ecore.EObject;
 import org.junit.Test;
 
@@ -171,6 +173,48 @@ public class ModelUtilTest extends AbstractNonTransactionalFeatureTest {
 		assertThat(dataObjects).isNotEmpty();
 		
 		assertThat(dataObjects).hasSize(3);
-	}	
-	
+	}
+
+	@Test
+	@DiagramResource("org/camunda/bpm/modeler/test/core/utils/ModelUtilTest.testGetFlowElementsContainer.bpmn")
+	public void testGetFlowElementsContainerForActivities() {
+
+		BaseElement businessObject  = Util.findBusinessObjectById(diagram, "CallActivity_2");
+		FlowElementsContainer parentContainer = ModelUtil.getFlowElementsContainer(businessObject);
+		assertThat(parentContainer.getId()).isEqualTo("SubProcess_2");
+
+		businessObject  = Util.findBusinessObjectById(diagram, "Task_2");
+		parentContainer = ModelUtil.getFlowElementsContainer(businessObject);
+		assertThat(parentContainer.getId()).isEqualTo("SubProcess_2");
+
+		businessObject  = Util.findBusinessObjectById(diagram, "CallActivity_1");
+		parentContainer = ModelUtil.getFlowElementsContainer(businessObject);
+		assertThat(parentContainer.getId()).isEqualTo("Process_1");
+
+		businessObject  = Util.findBusinessObjectById(diagram, "SubProcess_1");
+		parentContainer = ModelUtil.getFlowElementsContainer(businessObject);
+		assertThat(parentContainer.getId()).isEqualTo("Process_1");
+	}
+
+	@Test
+	@DiagramResource("org/camunda/bpm/modeler/test/core/utils/ModelUtilTest.testGetFlowElementsContainer.bpmn")
+	public void testGetFlowElementsContainerForEvents() {
+
+		BaseElement businessObject  = Util.findBusinessObjectById(diagram, "IntermediateThrowEvent_2");
+		FlowElementsContainer parentContainer = ModelUtil.getFlowElementsContainer(businessObject);
+		assertThat(parentContainer.getId()).isEqualTo("SubProcess_2");
+
+		businessObject  = Util.findBusinessObjectById(diagram, "IntermediateThrowEvent_1");
+		parentContainer = ModelUtil.getFlowElementsContainer(businessObject);
+		assertThat(parentContainer.getId()).isEqualTo("Process_1");
+	}
+
+	@Test
+	@DiagramResource("org/camunda/bpm/modeler/test/core/utils/ModelUtilTest.testGetFlowElementsContainer.bpmn")
+	public void testGetFlowElementsContainerForProcess() {
+
+		BaseElement businessObject = Util.findBusinessObjectById(diagram, "_Participant_3"); 
+		FlowElementsContainer parentContainer = ModelUtil.getFlowElementsContainer(businessObject);
+		assertThat(parentContainer).isNull();
+	}
 }
--- org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/ui/property/tabs/EventTabCompositeFactory.java
+++ org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/ui/property/tabs/EventTabCompositeFactory.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.camunda.bpm.modeler.core.utils.ModelUtil;
 import org.camunda.bpm.modeler.runtime.engine.model.ModelPackage;
 import org.camunda.bpm.modeler.ui.property.tabs.builder.BoundaryEventDefinitionComposite;
+import org.camunda.bpm.modeler.ui.property.tabs.builder.CompensateEventDefinitionPropertiesBuilder;
 import org.camunda.bpm.modeler.ui.property.tabs.builder.ErrorDefinitionPropertyBuilder;
 import org.camunda.bpm.modeler.ui.property.tabs.builder.IdPropertyBuilder;
 import org.camunda.bpm.modeler.ui.property.tabs.builder.MessageDefinitionPropertyBuilder;
@@ -14,9 +15,11 @@ import org.camunda.bpm.modeler.ui.property.tabs.builder.TimerEventDefinitionProp
 import org.camunda.bpm.modeler.ui.property.tabs.util.HelpText;
 import org.camunda.bpm.modeler.ui.property.tabs.util.PropertyUtil;
 import org.eclipse.bpmn2.BoundaryEvent;
+import org.eclipse.bpmn2.CompensateEventDefinition;
 import org.eclipse.bpmn2.ErrorEventDefinition;
 import org.eclipse.bpmn2.Event;
 import org.eclipse.bpmn2.EventDefinition;
+import org.eclipse.bpmn2.IntermediateThrowEvent;
 import org.eclipse.bpmn2.LinkEventDefinition;
 import org.eclipse.bpmn2.MessageEventDefinition;
 import org.eclipse.bpmn2.SignalEventDefinition;
@@ -47,6 +50,7 @@ public class EventTabCompositeFactory extends AbstractTabCompositeFactory<Event>
 		TimerEventDefinition timerDef = getEventDefinition(TimerEventDefinition.class, eventDefinitions);
 		SignalEventDefinition signalDef = getEventDefinition(SignalEventDefinition.class, eventDefinitions);
 		LinkEventDefinition linkDef = getEventDefinition(LinkEventDefinition.class, eventDefinitions);
+		CompensateEventDefinition compensateDef = getEventDefinition(CompensateEventDefinition.class, eventDefinitions);
 		
 		if (errorDef == null && event instanceof StartEvent) {
 			PropertyUtil.createText(section, parent, "Initiator", ModelPackage.eINSTANCE.getDocumentRoot_Initiator(), event);
@@ -71,7 +75,11 @@ public class EventTabCompositeFactory extends AbstractTabCompositeFactory<Event>
 		if (linkDef != null) {
 			createLinkDefinitionComposite(linkDef);
 		}
-		
+
+		if (compensateDef != null && (event instanceof IntermediateThrowEvent)) {
+			createCompensateDefinitionComposite(compensateDef);
+		}
+
 		if (event instanceof BoundaryEvent) {
 			createBoundaryEventComposite((BoundaryEvent) event);
 		}
@@ -111,5 +119,10 @@ public class EventTabCompositeFactory extends AbstractTabCompositeFactory<Event>
 	private void createLinkDefinitionComposite(LinkEventDefinition linkDef) {
 		new IdPropertyBuilder(parent, section, linkDef, "Link Id").create();
 		new NamePropertyBuilder(parent, section, linkDef, "Link Name", HelpText.LINK_EVENT_DEFINITION_NAME).create();
+	}
+
+	private void createCompensateDefinitionComposite(CompensateEventDefinition compensateDef) {
+		new IdPropertyBuilder(parent, section, compensateDef).create();
+		new CompensateEventDefinitionPropertiesBuilder(parent, section, compensateDef).create();
 	}
 }
