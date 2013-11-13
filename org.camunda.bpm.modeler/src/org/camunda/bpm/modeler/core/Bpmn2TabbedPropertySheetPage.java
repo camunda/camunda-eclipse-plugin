673dd69ddbbadcada44ce937b07873796d02e875
Nico Rehwaldt <nico.rehwaldt@camunda.com>
2013-11-13 16:44:15 +0100

    fix(property-panel): refresh asynchronously
    Related to CAM-1487

--- org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/core/Bpmn2TabbedPropertySheetPage.java
+++ org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/core/Bpmn2TabbedPropertySheetPage.java
@@ -1,6 +1,9 @@
 package org.camunda.bpm.modeler.core;
 
 import org.eclipse.graphiti.ui.editor.DiagramEditor;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.views.properties.tabbed.ITabbedPropertySheetPageContributor;
 import org.eclipse.ui.views.properties.tabbed.TabbedPropertySheetPage;
 
@@ -19,8 +22,25 @@ public class Bpmn2TabbedPropertySheetPage extends TabbedPropertySheetPage {
 	}
 	
 	@Override
+	public void refresh() {
+		super.refresh();
+	}
+	
+	@Override
 	public void resizeScrolledComposite() {
 		// TODO FIX SCROLL SPEED
 		super.resizeScrolledComposite();
+	}
+	
+	@Override
+	public void selectionChanged(final IWorkbenchPart part, final ISelection selection) {
+		
+		Display.getCurrent().asyncExec(new Runnable() {
+			
+			@Override
+			public void run() {
+				Bpmn2TabbedPropertySheetPage.super.selectionChanged(part, selection);
+			}
+		});
 	}
 }
