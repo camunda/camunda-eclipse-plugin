ca0c5dec338171da974f827527e27a6963219e3c
unknown <kristin.polenz@camunda.com>
2013-11-18 10:50:24 +0100

    test(callactivity/namespace): add test case for callActivity
    - testcase to check namespace of call activity attributes
    
    related to #CAM-1510

--- org.camunda.bpm.modeler.tests/src/org/camunda/bpm/modeler/test/integration/io/RoundtripTest.java
+++ org.camunda.bpm.modeler.tests/src/org/camunda/bpm/modeler/test/integration/io/RoundtripTest.java
@@ -1,11 +1,17 @@
 package org.camunda.bpm.modeler.test.integration.io;
 
+import static org.camunda.bpm.modeler.core.layout.util.ConversionUtil.point;
+import static org.camunda.bpm.modeler.test.util.operations.AddCallActivityOperation.addCallActivity;
 import static org.camunda.bpm.modeler.test.util.operations.AddStartEventOperation.addStartEvent;
 import static org.camunda.bpm.modeler.test.util.operations.DeleteElementOperation.deleteElement;
 import static org.fest.assertions.api.Assertions.assertThat;
 
+import org.camunda.bpm.modeler.runtime.engine.model.CallActivity;
 import org.camunda.bpm.modeler.test.integration.AbstractIntegrationTest;
 import org.camunda.bpm.modeler.test.util.Util;
+import org.eclipse.graphiti.mm.algorithms.styles.Point;
+import org.eclipse.graphiti.mm.pictograms.ContainerShape;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.mm.pictograms.Shape;
 import org.junit.Test;
 
@@ -48,7 +54,7 @@ public class RoundtripTest extends AbstractIntegrationTest {
 			.contains("startEvent")
 			.contains("process");
 	}
-	
+
 	@Test
 	public void shouldDeleteElementInModel() throws Exception{
 		// this test ensures that elements are deleted correctly
@@ -71,5 +77,50 @@ public class RoundtripTest extends AbstractIntegrationTest {
 			.doesNotContain("serviceTask")
 			.contains("startEvent")
 			.contains("endEvent");
+	}
+
+	@Test
+	public void shouldCreateCallActivityWithAttributes() throws Exception {
+
+		final EditorAndModel editorAndModel = open("RoundtripTest.emptyProcess.bpmn");
+		Diagram diagram = editorAndModel.getDiagram();
+
+		final Shape processShape = Util.findShapeByBusinessObjectId(diagram, "Process_1");
+
+		// add call activity to process
+		execute(new Behavior() {
+
+			public void run() throws Exception {
+				Point addPosition = point(10, 10);
+
+				addCallActivity(getDiagramTypeProvider())
+					.atLocation(addPosition)
+					.toContainer((ContainerShape) processShape)
+					.execute();
+			};
+		});
+
+		final CallActivity callActivity = Util.findBusinessObjectById(diagram, "CallActivity_1", CallActivity.class);
+		assertThat(callActivity).isNotNull();
+
+		// add some attributes to call activity
+		execute(new Behavior() {
+
+			public void run() throws Exception {
+				callActivity.setCalledElement("test");
+				callActivity.setCalledElementBinding("version");
+				callActivity.setCalledElementVersion(1);
+			}
+		});
+
+		String exportXml = save();
+
+		// check processes xml
+		assertThat(exportXml)
+				  .contains("Process_1")
+				  .contains("CallActivity_1")
+				  .contains("calledElement")
+				  .contains("camunda:calledElementBinding")
+				  .contains("camunda:calledElementVersion");
 	}
 }
