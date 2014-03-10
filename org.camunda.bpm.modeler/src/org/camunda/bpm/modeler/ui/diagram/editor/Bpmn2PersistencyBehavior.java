745d75eaa5ba74fdb0befb7bf5eb3745009f84d3
kristin polenz <kristin.polenz@camunda.com>
2014-03-10 11:28:12 +0100

    feat(definitions): Add exporter and exporterVersion
    - write exporter and exporterVersion to diagram definitions when it is not
      set
    
    related to #CAM-273

--- org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/ui/diagram/editor/Bpmn2PersistencyBehavior.java
+++ org.camunda.bpm.modeler/src/org/camunda/bpm/modeler/ui/diagram/editor/Bpmn2PersistencyBehavior.java
@@ -1,11 +1,19 @@
 package org.camunda.bpm.modeler.ui.diagram.editor;
 
+import java.util.Dictionary;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
+import org.camunda.bpm.modeler.core.utils.BusinessObjectUtil;
+import org.eclipse.bpmn2.Definitions;
+import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.xmi.XMLResource;
+import org.eclipse.emf.transaction.RecordingCommand;
+import org.eclipse.emf.transaction.TransactionalEditingDomain;
+import org.eclipse.graphiti.mm.pictograms.Diagram;
 import org.eclipse.graphiti.ui.editor.DefaultPersistencyBehavior;
 import org.eclipse.graphiti.ui.editor.DiagramEditor;
 
@@ -16,6 +24,30 @@ public class Bpmn2PersistencyBehavior extends DefaultPersistencyBehavior {
 	public Bpmn2PersistencyBehavior(DiagramEditor diagramEditor) {
 		super(diagramEditor);
 		editor = (Bpmn2Editor) diagramEditor;
+	}
+
+	@Override
+	protected Set<Resource> save(TransactionalEditingDomain editingDomain,
+			Map<Resource, Map<?, ?>> saveOptions) {
+
+		Diagram diagram = diagramEditor.getDiagramTypeProvider().getDiagram();
+		final Definitions definitions = BusinessObjectUtil.getFirstElementOfType(diagram, Definitions.class);
+
+		editingDomain.getCommandStack().execute(new RecordingCommand(editingDomain) {
+
+			@Override
+			protected void doExecute() {
+				Dictionary<String, String> bundleHeaders = Platform.getBundle("org.camunda.bpm.modeler").getHeaders();
+				if (definitions.getExporter() == null) {
+					definitions.setExporter(bundleHeaders.get("Bundle-Name"));
+				}
+				if (definitions.getExporterVersion() == null) {
+					definitions.setExporterVersion(bundleHeaders.get("Bundle-Version"));
+				}
+			}
+		});
+
+		return super.save(editingDomain, saveOptions);
 	}
 
 	@Override
